9b988419a119ebf6dd60bbbb3b197340fdae6ec2
John MacGregor <john.macgregor@gmail.com>
2023-08-26 10:58:23 -0400

    Add router to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Main } from './components/Main';
 
 export function App() {
   console.log('thing');
   return (
-     <div>
-        <h1>Hello!</h1>
-     </div>
+    <BrowserRouter>
+     <Routes>
+      <Route path={'/'} element={<Main />}>
+        <Route index element={<Main />} />
+      </Route>
+     </Routes>
+    </BrowserRouter>
   );
 }
